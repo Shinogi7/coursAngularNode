02df36d4da429e2b5e63014f17e91557d2069b9d
Thomas Giresse <thomas.giresse@hotmail.fr>
2023-05-25 14:17:20 +0200

    suppression des components en fr

--- courAngularNode/src/app/app.module.ts
+++ courAngularNode/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FichePersoAccueilComponent } from './components/fiche-perso-accueil/fiche-perso-accueil.component';
+import { BoutonComponent } from './components/bouton/bouton.component';
+import { CadreConnexionComponent } from './components/cadre-connexion/cadre-connexion.component';
+import { CadreInscriptionComponent } from './components/cadre-inscription/cadre-inscription.component';
+import { CardComponent } from './components/card/card.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FichePersoAccueilComponent,
+    BoutonComponent,
+    CadreConnexionComponent,
+    CadreInscriptionComponent,
+    CardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
