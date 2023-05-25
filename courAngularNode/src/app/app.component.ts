947f839b54a436f1d36d619ff18675d2d9b13461
Thomas Giresse <thomas.giresse@hotmail.fr>
2023-05-25 17:06:15 +0200

    commit fin de journée du 25 05 23

--- courAngularNode/src/app/app.component.ts
+++ courAngularNode/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'courAngularNode';
+  cards:string[]=["Vector", "Sammy", "Scooby"]
 }
--- courAngularNode/src/app/app.module.ts
+++ courAngularNode/src/app/app.module.ts
@@ -5,10 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { FichePersoAccueilComponent } from './components/fiche-perso-accueil/fiche-perso-accueil.component';
-import { BoutonComponent } from './components/bouton/bouton.component';
-import { CadreConnexionComponent } from './components/cadre-connexion/cadre-connexion.component';
-import { CadreInscriptionComponent } from './components/cadre-inscription/cadre-inscription.component';
 import { CardComponent } from './components/card/card.component';
 import { ButtonComponent } from './components/button/button.component';
 
@@ -17,10 +13,6 @@ import { ButtonComponent } from './components/button/button.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    FichePersoAccueilComponent,
-    BoutonComponent,
-    CadreConnexionComponent,
-    CadreInscriptionComponent,
     CardComponent,
     ButtonComponent
   ],
